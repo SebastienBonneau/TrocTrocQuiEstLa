63450731409bc69747718b01f233f29498f233d8
dgaillard2021 <dgaillard2021@27SE21-C4NZG5J.ad.campus-eni.fr>
2021-08-19 12:26:22 +0200

    commit 2

--- src/main/java/fr/eni/jee/projet/servlet/ServletSinscrire.java
+++ src/main/java/fr/eni/jee/projet/servlet/ServletSinscrire.java
@@ -13,11 +13,48 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletSinscrire")
 public class ServletSinscrire extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+	   
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		// 1 - On recupere les informations envoyees par le formulaire
+		String pseudo = request.getParameter("identifiant");
+		String prenom = request.getParameter("identifiant");
+		String telephone = request.getParameter("identifiant");
+		String codePostal = request.getParameter("identifiant");
+		String motDePasse = request.getParameter("identifiant");
+		String nom = request.getParameter("identifiant");
+		String email = request.getParameter("identifiant");
+		String rue = request.getParameter("identifiant");
+		String ville= request.getParameter("identifiant");
+		String confirmation= request.getParameter("identifiant");
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.getRequestDispatcher("/WEB-INF/Sinscrire.jsp").forward(request, response);
 	}
-
+	
+	
+	
 }
