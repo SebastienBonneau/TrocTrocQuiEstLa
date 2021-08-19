9add6f34b067e1519c06d55d03d26f1795a95de4
sbonneau2021 <sbonneau2021@27SE21-84NZG5J.ad.campus-eni.fr>
2021-08-19 09:55:18 +0200

    commit 19/08 accueil

---
+++ src/main/java/fr/eni/jee/projet/servlet/accueil.java
@@ -0,0 +1,24 @@
+package fr.eni.jee.projet.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class accueil
+ */
+@WebServlet("/accueil")
+public class accueil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("./accueil.jsp").forward(request, response);
+	}
+
+
+}
